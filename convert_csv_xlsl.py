0f2d392435e206dac7b5c540e3bc7561ff53436c
Aukenov Assanali <115568332+AsanX99999@users.noreply.github.com>
2024-01-24 14:36:33 +0600

    final

--- convert_csv_xlsl.py
+++ convert_csv_xlsl.py
@@ -1,26 +1,47 @@
+# import csv
+# from openpyxl import Workbook, load_workbook
+
+# # Load the existing Excel file
+# excel_file_path = 'test.xlsx'
+# workbook = load_workbook(excel_file_path)
+
+# # Select the second sheet (worksheet)
+# worksheet = workbook.worksheets[1]  # Index 0 represents the first sheet
+
+# # Open the CSV file
+# csv_file_path = 'final.csv'
+# with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
+#     reader = csv.reader(csvfile)
+
+#     # Skip the header row
+#     next(reader)
+
+#     # Iterate over the rows in the CSV file
+#     for row_index, row in enumerate(reader, start=2):
+#         # Write each value from the CSV row to the corresponding cell in the worksheet
+#         for column_index, value in enumerate(row, start=1):
+#             worksheet.cell(row=row_index, column=column_index, value=value)
+
+# # Save the modified Excel file
+# workbook.save(excel_file_path)
 import csv
-from openpyxl import Workbook, load_workbook
+from openpyxl import Workbook
 
-# Load the existing Excel file
-excel_file_path = 'test.xlsx'
-workbook = load_workbook(excel_file_path)
-
-# Select the second sheet (worksheet)
-worksheet = workbook.worksheets[1]  # Index 0 represents the first sheet
+# Create a new Excel workbook and a new worksheet
+workbook = Workbook()
+worksheet = workbook.active
 
 # Open the CSV file
-csv_file_path = 'main.csv'
+csv_file_path = 'final.csv'
 with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
 
-    # Skip the header row
-    next(reader)
-
     # Iterate over the rows in the CSV file
-    for row_index, row in enumerate(reader, start=2):
+    for row_index, row in enumerate(reader, start=1):
         # Write each value from the CSV row to the corresponding cell in the worksheet
         for column_index, value in enumerate(row, start=1):
             worksheet.cell(row=row_index, column=column_index, value=value)
 
-# Save the modified Excel file
+# Save the new Excel file
+excel_file_path = 'output.xlsx'
 workbook.save(excel_file_path)
