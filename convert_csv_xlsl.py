086ba887743b7ff8075ca482f8e67109a843774d
Aukenov Assanali <115568332+AsanX99999@users.noreply.github.com>
2024-01-25 22:50:35 +0600

    final

--- convert_csv_xlsl.py
+++ convert_csv_xlsl.py
@@ -1,29 +1,3 @@
-# import csv
-# from openpyxl import Workbook, load_workbook
-
-# # Load the existing Excel file
-# excel_file_path = 'test.xlsx'
-# workbook = load_workbook(excel_file_path)
-
-# # Select the second sheet (worksheet)
-# worksheet = workbook.worksheets[1]  # Index 0 represents the first sheet
-
-# # Open the CSV file
-# csv_file_path = 'final.csv'
-# with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
-#     reader = csv.reader(csvfile)
-
-#     # Skip the header row
-#     next(reader)
-
-#     # Iterate over the rows in the CSV file
-#     for row_index, row in enumerate(reader, start=2):
-#         # Write each value from the CSV row to the corresponding cell in the worksheet
-#         for column_index, value in enumerate(row, start=1):
-#             worksheet.cell(row=row_index, column=column_index, value=value)
-
-# # Save the modified Excel file
-# workbook.save(excel_file_path)
 import csv
 from openpyxl import Workbook
 
@@ -31,13 +5,20 @@ from openpyxl import Workbook
 workbook = Workbook()
 worksheet = workbook.active
 
+# Define the headers
+headers = ["Main website", "Item website", "Item", "Cost", "Currency", "Details", "Details", "Description", "Available"]
+
+# Write the headers to the first row of the worksheet
+for column_index, header in enumerate(headers, start=1):
+    worksheet.cell(row=1, column=column_index, value=header)
+
 # Open the CSV file
 csv_file_path = 'final.csv'
 with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
 
     # Iterate over the rows in the CSV file
-    for row_index, row in enumerate(reader, start=1):
+    for row_index, row in enumerate(reader, start=2):  # start from 2 because 1 is used by headers
         # Write each value from the CSV row to the corresponding cell in the worksheet
         for column_index, value in enumerate(row, start=1):
             worksheet.cell(row=row_index, column=column_index, value=value)
